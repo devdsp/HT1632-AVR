206275f27c502b6555a22bae49afb436b08354c1
Adam Thomas <adam.lloyd@gmail.com>
2010-03-16 15:49:57 +1100

    added license details

--- MHV_io_ArduinoDuemilanove328p.h
+++ MHV_io_ArduinoDuemilanove328p.h
@@ -3,6 +3,23 @@
  *
  *  Created on: 21/02/2010
  *      Author: deece
+
+Copyright (C) 2010 Alastair D'Silva
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
  */
 
 #ifndef MHV_IO_ARDUINO_DUIMILANOVE_328P_H_
